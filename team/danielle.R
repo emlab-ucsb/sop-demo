e119a9fe931334a3fb905cf0651ada9a12eda767
Danielle Ferraro <49916907+danielleferraro@users.noreply.github.com>
2026-06-24 12:33:45 -0700

    Add Danielle's greeting function (#5)
    * Add Danielle's greeting function
    
    * Add friendlier greeting per Tracey's review
    
    * Fix syntax error in greeting message
    
    this looks friendly and historical, and i just added a closing parenthesis
    
    ---------
    
    Co-authored-by: Tracey Mangin <traceymangin@gmail.com>

---
+++ team/danielle.R
@@ -0,0 +1,11 @@
+# team/danielle.R
+
+greet <- function(name) {
+  paste0(
+    "Welcome to the underrated state of Delaware, ",
+    name,
+    "! The first state (and neighbor of New Jersey is glad you're here.)"
+  )
+}
+
+greet("Tracey")

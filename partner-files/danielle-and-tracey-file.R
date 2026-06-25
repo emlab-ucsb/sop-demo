13e8fe06c3f3be6bb6216b57e23839277d3e2e9c
Tracey Mangin <traceymangin@gmail.com>
2026-06-25 14:25:57 -0700

    created activity message function for danielle and tracey (#10)
    created shared file for danielle and tracey

---
+++ partner-files/danielle-and-tracey-file.R
@@ -0,0 +1,8 @@
+## tracey and danielle's shared function
+
+activity_message <- function(activity) {
+  message <- paste("Hey! I just completed", activity, "on my fitness tracker!")
+  return(message)
+}
+
+activity_message("jog/walk 5 miles") # Example usage of the function

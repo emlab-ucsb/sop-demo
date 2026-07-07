3a7eef59871edbb5f099159c53a929f3006202d9
Robert Heilmayr <rheilmayr@ucsb.edu>
2026-07-07 11:24:36 -0700

    I don't like scalliions

--- conflict-files/partners-5.R
+++ conflict-files/partners-5.R
@@ -1,7 +1,11 @@
 ## partners-5: everything bagels
 
 bagel_review <- function(spread) {
-  paste0("An everything bagel with ", spread, " is the only correct breakfast. I will not be taking questions.")
+  paste0(
+    "An everything bagel with ",
+    spread,
+    " is the only correct breakfast. I will not be taking questions."
+  )
 }
 
-bagel_review("scallion cream cheese")
+bagel_review("whipped cream cheese")

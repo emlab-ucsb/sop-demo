6938cd06f3b1bcabeb7b9f8214d9d7807f3b0d78
Danielle Ferraro <49916907+danielleferraro@users.noreply.github.com>
2026-07-07 11:27:39 -0700

    Change to kilometers (#38)

--- conflict-files/partners-4.R
+++ conflict-files/partners-4.R
@@ -1,7 +1,11 @@
 ## partners-4: biking
 
 bike_excuse <- function(miles) {
-  paste0("Yeah I only biked ", miles, " miles today, but it was uphill. Both ways. Into the wind.")
+  paste0(
+    "Yeah I only biked ",
+    miles,
+    " kilometers today, but it was uphill. Both ways. Into the wind."
+  )
 }
 
 bike_excuse(3)

d10edb0fad5d2035cb1afa65ecb6f930ffb625e5
lennon-thomas <lennon.rose.thomas@gmail.com>
2026-07-07 11:24:49 -0700

    I came clean on my lie

--- conflict-files/partners-1.R
+++ conflict-files/partners-1.R
@@ -1,7 +1,7 @@
 ## partners-1: scallops
 
 scallop_brag <- function(count) {
-  paste0("I just seared ", count, " scallops and not one of them stuck to the pan. A perfect day.")
+  paste0("I just seared ", count, " scallops and ALL of them stuck to the pan. A perfect day.")
 }
 
 scallop_brag(12)

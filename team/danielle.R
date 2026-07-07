1fe087cd2680854a3a87e799c43e18b32069ab7a
Danielle Ferraro <dferraro@ucsb.edu>
2026-07-07 11:03:00 -0700

    Make Danielle's greeting friendlier per Tracey's review

--- team/danielle.R
+++ team/danielle.R
@@ -1,7 +1,7 @@
 # team/danielle.R
 
 greet <- function(name) {
-  paste0("Welcome to the underrated state of Delaware", name)
+  paste0("Welcome to the underrated state of Delaware ", name, "!!!")
 }
 
 greet("Tracey")
